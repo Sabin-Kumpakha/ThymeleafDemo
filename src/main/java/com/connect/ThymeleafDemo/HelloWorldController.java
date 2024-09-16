3fb0a94dd075f4fc64a3157e1e928fd0b6cd13fa
sabin <hellosabin7@gmail.com>
2024-09-16 15:59:26 +0545

    done

--- src/main/java/com/connect/ThymeleafDemo/HelloWorldController.java
+++ src/main/java/com/connect/ThymeleafDemo/HelloWorldController.java
@@ -4,12 +4,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Controller
 public class HelloWorldController {
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String hello(Model model){
+        String greeting;
+        Date now = new Date();
+        int hour = now.getHours();
+
+        if(hour < 12){
+            greeting = "Good Morning";
+        } else if (hour < 18) {
+            greeting = "Good Afternoon";
+        } else if (hour < 21) {
+            greeting = "Good Evening";
+        } else {
+            greeting = "Good Night";
+        }
+
         model.addAttribute("message", "Hello World!!!");
+        model.addAttribute("greeting", greeting);
         return "helloWorld";
     }
 }
